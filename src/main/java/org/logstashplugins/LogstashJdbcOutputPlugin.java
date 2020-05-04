582e389b58f11eec3e8c2f3b3c119a802007213c
Safak Topcu <s.safak.topcu@gmail.com>
2020-05-04 13:59:13 +0300

    add DLQ support

--- src/main/java/org/logstashplugins/LogstashJdbcOutputPlugin.java
+++ src/main/java/org/logstashplugins/LogstashJdbcOutputPlugin.java
@@ -5,21 +5,24 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.logstash.DLQEntry;
+import org.logstash.common.DeadLetterQueueFactory;
+import org.logstash.common.io.DeadLetterQueueWriter;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
+
 
 @LogstashPlugin(name = "logstash_jdbc_output_plugin")
 public class LogstashJdbcOutputPlugin implements Output {
     
-    private static final Logger logger = LogManager.getLogger(LogstashJdbcOutputPlugin.class);
+    private Logger logger;
     
     public static final PluginConfigSpec<String> CONNECTION_STRING =
             PluginConfigSpec.stringSetting("connectionString", "");
@@ -44,6 +47,12 @@ public class LogstashJdbcOutputPlugin implements Output {
 
     public static final PluginConfigSpec<Long> MIN_IDLE =
             PluginConfigSpec.numSetting("minIdle", 10);
+
+    public static final PluginConfigSpec<String> DEAD_LETTER_QUEUE_PATH =
+            PluginConfigSpec.stringSetting("deadLetterQueuePath");
+
+    public static final PluginConfigSpec<Long> DEAD_LETTER_QUEUE_MAX_SIZE =
+            PluginConfigSpec.numSetting("deadLetterQueueMaxSize",1000000);
     
     private final String id;
     private final CountDownLatch done = new CountDownLatch(1);
@@ -54,10 +63,20 @@ public class LogstashJdbcOutputPlugin implements Output {
     private String sqlStatement;
     private List<String> orderedEventParameterNames;
 
+    private Optional<DeadLetterQueueWriter> deadLetterQueueWriterOptional;
+    
     private HikariDataSource dataSource;
 
     public LogstashJdbcOutputPlugin(final String id, final Configuration config, final Context context) {
-        
+        logger = context.getLogger(this);
+        String deadLetterQueuePath = config.get(DEAD_LETTER_QUEUE_PATH);
+        if (deadLetterQueuePath != null){
+            Long deadLetterQueueMaxSize = config.get(DEAD_LETTER_QUEUE_MAX_SIZE);
+            logger.info("Configuring DLQ with deadLetterQueuePath {}, deadLetterQueueMaxSize {}",deadLetterQueuePath, deadLetterQueueMaxSize);
+            deadLetterQueueWriterOptional = Optional.ofNullable(DeadLetterQueueFactory.getWriter(id,deadLetterQueuePath,deadLetterQueueMaxSize));
+        }else{
+            deadLetterQueueWriterOptional = Optional.empty();
+        }
         this.id = id;
         maxBatchSize = config.get(MAX_BATCH_SIZE_CONFIG);
         String connectionString = config.get(CONNECTION_STRING);
@@ -89,52 +108,80 @@ public class LogstashJdbcOutputPlugin implements Output {
     public void output(final Collection<Event> events) {
         long startTime = System.currentTimeMillis();
         logger.debug(()->"Number of events: "+events.size()+" ");
-        
+        List<Event> batchedEvents = new ArrayList<>();
         try (
                 Connection connection = dataSource.getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement(sqlStatement); 
                 )
         {
             connection.setAutoCommit(false);
-            int currentBatchSize = 0;
             for (Event event : events){
-                for (int i = 0; i< orderedEventParameterNames.size() ; i++){
-                    Object field = event.getField(orderedEventParameterNames.get(i));
-                    if (field instanceof Integer){
-                        preparedStatement.setInt(i+1, (Integer) field);
-                    }else if(field instanceof Double){
-                        preparedStatement.setDouble(i+1, (Double) field);
-                    }else if(field instanceof BigDecimal){
-                        preparedStatement.setBigDecimal(i+1, (BigDecimal) field);
-                    }else if(field instanceof Long){
-                        preparedStatement.setLong(i+1, (Long) field);
-                    }else if(field instanceof String){
-                        preparedStatement.setString(i+1, (String) field);
-                    }else{
-                        preparedStatement.setObject(i+1, field);
+                try {
+                    for (int i = 0; i< orderedEventParameterNames.size() ; i++){
+                        Object field = event.getField(orderedEventParameterNames.get(i));
+                        if (field instanceof Integer){
+                            preparedStatement.setInt(i+1, (Integer) field);
+                        }else if(field instanceof Double){
+                            preparedStatement.setDouble(i+1, (Double) field);
+                        }else if(field instanceof BigDecimal){
+                            preparedStatement.setBigDecimal(i+1, (BigDecimal) field);
+                        }else if(field instanceof Long){
+                            preparedStatement.setLong(i+1, (Long) field);
+                        }else if(field instanceof String){
+                            preparedStatement.setString(i+1, (String) field);
+                        }else{
+                            preparedStatement.setObject(i+1, field);
+                        }
                     }
+                    batchedEvents.add(event);
+                    preparedStatement.addBatch();
+                    
+                    if (batchedEvents.size() >= maxBatchSize){
+                        logger.debug("Committing {} records.",batchedEvents.size());
+                        preparedStatement.executeBatch();
+                        connection.commit();
+                        batchedEvents.clear();
+                    }
+                } catch (SQLException e) {
+                    logger.error("SQL Exception occured. Adding to DLQ.",e);
+                    addToDLQ(batchedEvents,e.getMessage());
                 }
-                preparedStatement.addBatch();
-                currentBatchSize++;
-                if (currentBatchSize >= maxBatchSize){
-                    logger.debug("Committing {} records.",currentBatchSize);
-                    preparedStatement.executeBatch();
-                    currentBatchSize = 0;
-                    connection.commit();
-                }
+
             }
-            if (currentBatchSize > 0 && !preparedStatement.isClosed()){
-                logger.debug("LAST BATCH OP Committing {} records.",currentBatchSize);
+            if (batchedEvents.size() > 0 && !preparedStatement.isClosed()){
+                logger.debug("LAST BATCH OP Committing {} records.",batchedEvents.size());
                 preparedStatement.executeBatch();
                 connection.commit();
+                batchedEvents.clear();
             }
             
         } catch (SQLException e) {
-            logger.error("SQL Exception occurred.", e);
+            logger.error("SQL Exception occured. Adding to DLQ.",e);
+            addToDLQ(batchedEvents,e.getMessage());
         }
         logger.debug(()->"Executed: "+events.size()+" in "+(System.currentTimeMillis() - startTime)+" milliseconds.");
     }
 
+    private void addToDLQ(List<Event> events,String reason){
+        deadLetterQueueWriterOptional.orElseThrow(() -> new RuntimeException("DLQ is not configured"));
+        DeadLetterQueueWriter deadLetterQueueWriter = deadLetterQueueWriterOptional.get();
+        for (Event batchedEvent : events){
+            org.logstash.Event logstashEvent = new org.logstash.Event();
+            logstashEvent.append(batchedEvent);
+            try {
+                deadLetterQueueWriter.writeEntry(
+                        logstashEvent,
+                        "logstash_jdbc_output_plugin",
+                        "logstash_jdbc_output_plugin",
+                        reason
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException("DLQ writer is not available.",ex);
+            }
+        }
+        events.clear();
+    }
+    
     @Override
     public void stop() {
         stopped = true;
@@ -150,6 +197,8 @@ public class LogstashJdbcOutputPlugin implements Output {
     public Collection<PluginConfigSpec<?>> configSchema() {
         // should return a list of all configuration options for this plugin
         return Arrays.asList(
+                DEAD_LETTER_QUEUE_PATH,
+                DEAD_LETTER_QUEUE_MAX_SIZE,
                 CONNECTION_STRING, 
                 MAX_BATCH_SIZE_CONFIG,
                 SQL_STATEMENT,
